cd8249f79ae453446faed6eb94b0a0b1915a7e2e
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-02-26 14:45:13 -0300

    first push. Changing ListaRestaurantes listing from hard-code to API requests

---
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+  const apiUrl = 'http://localhost:8000/api/v1/restaurantes/';
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
+
+  useEffect(() => {
+    // obter restaurantes
+    axios
+      .get<IPaginacao<IRestaurante>>(apiUrl)
+      .then((response) => {
+        setRestaurantes(response.data.results);
+        setProximaPagina(response.data.next);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const verMais = () => {
+    axios
+      .get<IPaginacao<IRestaurante>>(apiUrl)
+      .then((response) => {
+        setRestaurantes([...restaurantes, ...response.data.results]);
+        setProximaPagina(response.data.next);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}
+      {proximaPagina && <button onClick={verMais}>ver mais</button>}
+    </section>
+  );
+};
+
+export default ListaRestaurantes;
