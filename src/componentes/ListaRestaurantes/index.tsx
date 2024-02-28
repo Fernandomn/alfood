f1992ba2cfae65a47475ece5ab526081d0f8be44
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-02-28 15:14:17 -0300

    Adding feature flag to switch between 'ver mais' and 'proxima pagina/pagina anterior' buttons. Refactoring to avoid overuse of setState hooks

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -6,28 +6,34 @@ import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
-  const apiUrl = 'http://localhost:8000/api/v1/restaurantes/';
+  const [data, setData] = useState<IPaginacao<IRestaurante>>();
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
-  const [apiUrlProximaPagina, setApiUrlProximaPagina] = useState('');
+  const progressivePaginationFlag = true;
+  const apiUrl = 'http://localhost:8000/api/v1/restaurantes/';
+  const listaRestaurantes: IRestaurante[] = data?.results || [];
+  const apiUrlPaginaAnterior: string = data?.previous || '';
+  const apiUrlProximaPagina: string = data?.next || '';
 
   useEffect(() => {
-    // obter restaurantes
-    axios
-      .get<IPaginacao<IRestaurante>>(apiUrl)
-      .then((response) => {
-        setRestaurantes(response.data.results);
-        setApiUrlProximaPagina(response.data.next);
-      })
-      .catch((error) => console.error(error));
+    carregarDados(apiUrl);
   }, []);
 
-  const verMais = () => {
+  const carregarDados = (url: string) => {
+    axios
+      .get<IPaginacao<IRestaurante>>(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const verMais = (): void => {
     axios
       .get<IPaginacao<IRestaurante>>(apiUrlProximaPagina)
       .then((response) => {
-        setRestaurantes([...restaurantes, ...response.data.results]);
-        setApiUrlProximaPagina(response.data.next);
+        const newData = { ...response.data };
+        newData.results = [...listaRestaurantes, ...response.data.results];
+        setData(newData);
       })
       .catch((error) => console.error(error));
   };
@@ -37,10 +43,32 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
-      {restaurantes?.map((item) => (
+      {listaRestaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {apiUrlProximaPagina && <button onClick={verMais}>ver mais</button>}
+      {progressivePaginationFlag && apiUrlProximaPagina && (
+        <button onClick={verMais}>ver mais</button>
+      )}
+      {!progressivePaginationFlag && (
+        <>
+          {
+            <button
+              onClick={() => carregarDados(apiUrlPaginaAnterior)}
+              disabled={!apiUrlPaginaAnterior}
+            >
+              Página Anterior
+            </button>
+          }
+          {
+            <button
+              onClick={() => carregarDados(apiUrlProximaPagina)}
+              disabled={!apiUrlProximaPagina}
+            >
+              Próxima Página
+            </button>
+          }
+        </>
+      )}
     </section>
   );
 };
